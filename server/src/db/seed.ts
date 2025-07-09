9ae1e379448fc65a5f2f0cea06f6a5f4139a3e5e
patpolts <patriciapoltts@gmail.com>
2025-07-09 20:11:35 -0300

    feat(API): implemented new routes and seeds - add create room and question - add get questions

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -1,6 +1,7 @@
 import {reset, seed} from 'drizzle-seed'
 import { db, sql } from './connection.ts'
 import { schema } from './schema/index.ts'
+import { questions } from './schema/questions.ts';
 
 (async () => {
   await reset(db, schema);
@@ -13,6 +14,9 @@ import { schema } from './schema/index.ts'
           name: f.companyName(),
           description: f.loremIpsum(),
         },
+      },
+      questions: {
+        count: 15,
       },
     }
   });
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,12 @@
 import {fastify} from "fastify"
+import { sql } from "./db/connection.ts"
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod"
 import{fastifyCors} from '@fastify/cors'
-// import { sql } from "./db/connection.ts"
 import { env } from "./env.ts"
 import { getRoomsRoute } from "./http/routes/get-rooms.ts"
+import { createRoomRoute } from "./http/routes/create-room.ts"
+import { getRoomQuestionsRoute } from "./http/routes/get-room-questions.ts"
+import { createQuestionRoute } from "./http/routes/create-question.ts"
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -19,5 +22,8 @@ app.get('/heath', () => {
 })
 
 app.register(getRoomsRoute)
+app.register(getRoomQuestionsRoute)
+app.register(createRoomRoute)
+app.register(createQuestionRoute)
 
 app.listen({port: env.PORT})
